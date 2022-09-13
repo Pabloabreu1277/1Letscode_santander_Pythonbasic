a08bae8900276c6427acc28c1774315c3f55b22d
PabloAbreu <108098406+pabloloko1277@users.noreply.github.com>
2022-09-12 22:35:00 -0300

    Add files via upload

---
+++ 07 - Estruturas_Repetição_While_letscode.py
@@ -0,0 +1,43 @@
+contador = 0
+
+while contador < 10:
+    contador += 1
+    if contador ==1:
+        print(contador,"item limpo ")
+    else:
+        print(contador,"itens limpos ")
+    
+print("final da lavagem das louças")
+#######################################################################################
+contador = 0
+while True:
+    if contador < 20 :
+        contador += 1
+        if contador ==1:
+            print(contador,"item limpo ")
+        else:
+            print(contador,"itens limpos ")
+    else:
+        break
+    
+print("final da lavagem das louças")
+######################################################################################
+
+text =input("digite a sua senha:")
+
+while text != "letscode":
+    text = input("senha invalida tente novamente")
+print("acesso permitido")
+####################################################################################
+
+contador = 0
+
+while contador < 30 :
+    contador += 1
+    if contador ==1:
+        continue
+        
+    
+    print(contador,"itens limpos ")
+    
+print("final da lavagem das louças")
